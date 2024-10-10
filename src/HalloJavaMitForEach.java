0bf7b747ad7e99c580ce3acae50cfc135b519469
daToni06 <Benjiro6106@gmail.com>
2024-10-10 19:05:02 +0200

    Completed "Beispiel 2"

--- src/HalloJavaMitForEach.java
+++ src/HalloJavaMitForEach.java
@@ -3,7 +3,6 @@ import java.util.List;
 
 public class HalloJavaMitForEach {
 
-
     public static void main(String[] args) {
         List<String> outputForEach = new ArrayList<>();
         for(String s : outputForEach){
