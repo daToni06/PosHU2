32bc07064badda59f7f1d116c3aaba00610ef635
daToni06 <Benjiro6106@gmail.com>
2024-10-10 19:02:15 +0200

    Added Class "HalloJavaMitForEach"

---
+++ src/HalloJavaMitForEach.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavaMitForEach {
+
+
+    public static void main(String[] args) {
+        List<String> outputForEach = new ArrayList<>();
+        for(String s : outputForEach){
+            System.out.println(s);
+        }
+        outputForEach.forEach(s -> System.out.println(s));
+
+    }
+}
